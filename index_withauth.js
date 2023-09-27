452ca95ad51fa78a03635599eca0279e702e66b2
ducngg <ducng1311@gmail.com>
2023-09-27 14:10:20 -0400

    Update GET POST PUT DELETE methods

--- index_withauth.js
+++ index_withauth.js
@@ -4,19 +4,19 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 
 const app = express();
-const PORT =5000;
+const PORT = 5000;
 
-app.use(session({secret:"fingerpint",resave: true, saveUninitialized: true}))
+app.use(session({secret: "fingerpint", resave: true, saveUninitialized: true}))
 
 app.use(express.json());
 
-app.use("/user", (req,res,next)=>{
+app.use("/user", (req, res, next)=>{
 // Middleware which tells that the user is authenticated or not
    
    if(req.session.authorization) {
        let token = req.session.authorization['accessToken']; // Access Token
        
-       jwt.verify(token, "access",(err,user)=>{
+       jwt.verify(token, "access", (err, user) => {
            if(!err){
                req.user = user;
                next();
@@ -37,13 +37,15 @@ app.post("/login", (req,res) => {
     if (!user) {
         return res.status(404).json({message: "Body Empty"});
     }
-    let accessToken = jwt.sign({
+    let accessToken = jwt.sign(
+      {
         data: user
-      }, 'access', { expiresIn: 60 * 60 });
+      }, 
+      'access',
+      { expiresIn: 60 * 60 }
+    );
 
-      req.session.authorization = {
-        accessToken
-    }
+    req.session.authorization = { accessToken };
     return res.status(200).send("User successfully logged in");
 });
 
--- routes/users.js
+++ routes/users.js
@@ -25,35 +25,62 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify({users}, null, 2));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  const filtered_users = users.filter((user) => user.email === email);
+  if (filtered_users.length > 0) {
+    res.send(filtered_users);
+  } else {
+    res.send("No user with that email!");
+  }
 });
-
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  users.push({
+    "firstName":req.query.firstName,
+    "lastName":req.query.lastName,
+    "email":req.query.email,
+    "DOB":req.query.DOB
+  });
+  res.send("User "+ (req.query.firstName) + " has been added!")
 });
 
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
-});
+    const email = req.params.email;
+    let filtered_users = users.filter((user) => user.email === email);
+    if (filtered_users.length > 0) {
+        let filtered_user = filtered_users[0];
+        if (req.query.DOB) {
+            filtered_user.DOB = req.query.DOB;
+        }
+        if (req.query.firstName) {
+            filtered_user.DOB = req.query.firstName;
+        }
+        if (req.query.lastName) {
+            filtered_user.DOB = req.query.lastName;
+        }
+        users = users.filter((user) => user.email != email);
+        users.push(filtered_user);
+        res.send(`User with the email ${email} updated.`);
+    }
+    else {
+        res.send("Unable to find user!");
+    }
+  });
 
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  users = users.filter((user) => user.email != email);
+  res.send(`User with the email  ${email} deleted.`);
 });
 
 module.exports=router;
